a3e6933b87222c177240450af31722eeebb540b9
nari19 <kazuki7793996@gmail.com>
2019-07-03 22:29:36 +0900

    拍手ボタン

--- src/views/content/Container.js
+++ src/views/content/Container.js
@@ -8,9 +8,9 @@ class Container extends React.Component {
     render() {
         return(
             <main>
-                <p className="content-title">賞賛メッセージの投稿</p>
+                <p className="content-title h5 text-dark">賞賛メッセージの投稿</p>
                 <Post />
-                <p className="content-title">みんなの投稿</p>
+                <p className="content-title h5 text-dark">みんなの投稿</p>
                 <Viewer />
                 <Viewer />
                 <Viewer />
--- src/views/content/Viewer.js
+++ src/views/content/Viewer.js
@@ -9,12 +9,14 @@ class Viewer extends React.Component {
                 <Card>
                     <Row><Col>
                         <img src={User} alt="User" height="38px"/>&nbsp;kazuki
-                        <i className="fa fa-arrow-right secondary"></i>
+                        <i className="fa fa-arrow-right"></i>
                         <img src={User} alt="User" height="38px"/>&nbsp;kazuki
                     </Col></Row>
                     <hr />
                     <CardBody>
                         <CardText>今日も元気にお仕事なさってますね。こっちまで明るい気持ちになります!</CardText>
+                        <i className="fa fa-sign-language h3"></i>&nbsp;&nbsp;
+                        <span className="h5 text-dark">5</span>
                         <CardText className="pull-right text-muted h6">2019/04/23 11:51</CardText>
                     </CardBody>
                 </Card>
