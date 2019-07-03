ab4d5704b7fc5de35fb8e96aee338fae5bf2adc3
nari19 <kazuki7793996@gmail.com>
2019-07-03 15:16:47 +0900

    0703

--- src/views/Header.js
+++ src/views/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../assets/style/Header.scss';
 
 import { Form, Input, Container, Row, Col } from 'reactstrap';
+import User from '../assets/img/User/f_f_event_97_s512_f_event_97_0bg.png';
 
 class Header extends React.Component {
     render() {
@@ -9,17 +10,10 @@ class Header extends React.Component {
             <header>
                 <Container>
                     <Row>
-                        <Col className="column">
-                            <Form className="form">
-                                <Input type="search" placeholder="&#xf002; Search by Channel" />                                
-                            </Form>
-                        </Col>
-                        <Col className="column" xs="12" md="3">
-                            <div className="menus">
-                                <div className="menu-wrapper"><a href="#"><i className="fa fa-user-circle menu" aria-hidden="true"></i></a></div>
-                                <div className="menu-wrapper"><a href="#"><i className="fa fa-pie-chart menu" aria-hidden="true"></i></a></div>
-                                <div className="menu-wrapper"><a href="#"><i className="fa fa-video-camera menu" aria-hidden="true"></i></a></div>
-                            </div>
+                        <Col>
+                            <img src={User} alt="User" height="60px"/>
+                            aaaaaaaaaaaaaaaaa
+                            <Form className="form"><Input type="search" placeholder="&#xf002; Search by Channel" /></Form>
                         </Col>
                     </Row>
                 </Container>
